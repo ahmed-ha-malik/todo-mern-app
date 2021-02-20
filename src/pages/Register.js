a8d02eef00ccbdeaa84ea9a440f142e8d010f2ff
Ahmed Malik <ahmed.ha.malik@gmail.com>
2021-02-19 23:30:43 -0500

    Checking to see if user already exists, otherwise redirecting to homepage

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,10 +1,21 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+import { useHistory } from 'react-router-dom';
+
+const handleErrors = async (response) => {
+    if(!response.ok) {
+        const {message} = await response.json();
+        console.log("message: ", message);
+        throw Error(message);
+    }
+    return response.json();
+}
 
 function Register() {
 
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
-
+    const [error, setError] = useState(''); 
+    
     const register = (e) => {
         e.preventDefault();
         fetch('http://localhost:4000/register', {
@@ -16,13 +27,23 @@ function Register() {
                 username,
                 password,
             }),
+        })
+        .then(handleErrors)
+        .then(() => {
+            history.push('/');
+        })
+        .catch((error) => {
+            setError(error.message);
         });
     };
+
+    const history = useHistory();
 
     return (
         <div>
             <h1>Register</h1>
-            <form onClick={register}>
+            {error && (<span style={{color: 'red'}}>{error}</span>)}
+            <form onSubmit={register}>
                 <input onChange={(e) => setUsername(e.target.value)} placeholder='username' />
                 <br/>
                 <input onChange={(e) => setPassword(e.target.value)} type='password' placeholder='password' />
